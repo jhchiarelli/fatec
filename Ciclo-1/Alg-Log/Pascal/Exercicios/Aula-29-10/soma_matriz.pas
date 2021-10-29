a9ad197033265077e57634b2ec8faf1d1f672a89
Jorge Henrique <jhcchiarelli@gmail.com>
2021-10-29 16:59:38 -0300

    Changed type val to Decimal

--- Ciclo-1/Alg-Log/Pascal/Exercicios/Aula-29-10/soma_matriz.pas
+++ Ciclo-1/Alg-Log/Pascal/Exercicios/Aula-29-10/soma_matriz.pas
@@ -1,10 +1,11 @@
 Program Exerc_SomaMatrizes;
 Uses crt;
 Type
-  Mat = array [1..3, 1..3] of integer;
+  Mat = array [1..3, 1..3] of real;
 var
   MA, MB, MC: Mat;
   i, j: integer;
+  soma, valorMA, valorMB: real;
 begin
   clrscr;
   writeln ('SOMA DAS MATRIZES');
@@ -23,13 +24,24 @@ begin
    end;
 
   clrscr;
+  soma:= 0.0;
+  valorMA:= 0.0;
+  valorMB:= 0.0;
   writeln ('EXIBINDO DADOS DAS MATRIZES E A SOMA DELAS');
   for i := 1 to 3 do
    for j := 1 to 3 do
     begin
-     writeln('MATRIZ MA: ', MA[i,j]);
-     writeln('MATRIZ MB: ', MA[i,j]);
-     writeln('SOMA DE MA + MB: ', MC[i,j]);
+     writeln('MATRIZ A LINHA: ', i, ' COLUNA: ', j, ' VALOR: ', MA[i,j]:8:2);
+     writeln('MATRIZ B LINHA: ', i, ' COLUNA: ', j, ' VALOR: ', MB[i,j]:8:2);
+     valorMA:= valorMA+MA[i,j];
+     valorMB:= valorMB + MB[i,j];
+     soma:= soma + MC[i,j];
     end;
+    //writeln;
+    writeln('--------------------------------------------');
+    writeln('VALOR TOTAL MATRIZ A......: ', valorMA:15:2);
+    writeln('VALOR TOTAL MATRIZ B......: ', valorMB:15:2);
+    writeln('SOMA DA MATRIZ A+B TOTAL..: ', soma:15:2);
+    writeln('--------------------------------------------');
   readkey;
 end.
