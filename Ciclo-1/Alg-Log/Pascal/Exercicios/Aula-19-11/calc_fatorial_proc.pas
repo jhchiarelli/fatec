a37b0e4777f9bdb111e6b4f5916544f18b6e7510
Jorge Henrique <jhcchiarelli@gmail.com>
2021-11-19 12:04:33 -0300

    Calc Fatorial Procedure

---
+++ Ciclo-1/Alg-Log/Pascal/Exercicios/Aula-19-11/calc_fatorial_proc.pas
@@ -0,0 +1,27 @@
+
+program CalculoFatorial;
+Uses crt;
+procedure calc_fatorial(x: Integer); 
+Var
+   N, Cont, Fat: Integer;
+Begin
+   Cont := 1;
+   Fat  := 1;
+   Clrscr;
+   N:= x;
+   //Read(N);
+   While Cont <= N Do
+       Begin
+          Fat  := Fat * Cont;
+          Cont := Cont + 1;
+       End;
+   Writeln('O Fatorial do numero ', N, ' ‚: ', Fat);
+    
+end; 
+ 
+begin
+
+  writeln ('Calculo Fatorial');
+  calc_fatorial(3);
+  //readkey;
+end.
